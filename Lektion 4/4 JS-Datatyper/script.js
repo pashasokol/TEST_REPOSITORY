ea579eb48971702bdc9b2846031deb1d1d2af2da
Pavel <sokoliklive@gmail.com>
2021-09-23 17:53:05 +0200

    new

---
+++ Lektion 4/4 JS-Datatyper/script.js
@@ -0,0 +1,93 @@
+let doNotUse = "Pavel";
+let useThis = 'Pavel';
+let number = 28;
+
+
+
+console.log(useThis, number);
+
+
+let string = ' Mitt namn är ' + useThis + ' och jag är ' + number + ' år gammal.';
+let template =  `Mitt namn är ${useThis} och jag är ${number} år gammal`;
+let quote = "Mitt namn är " + doNotUse;
+
+
+
+//let firstName = 'Pavel';
+// let lastName = 'Sakalouski';
+
+// let fullName =  `${firstName} ${lastName}` ;
+
+//console.log(fullName);
+
+let myName = 'Pavel Sakalouski';
+
+/* console.log(myName.length);
+console.log(myName[0]);
+console.log(myName[1]);
+console.log(myName.indexOf('l'));
+
+console.log(myName.toUpperCase());
+console.log(myName.toLowerCase());
+
+myName = myName.toUpperCase();
+
+console.log(myName);
+
+console.log(myName.split('A'));
+
+*/
+
+/*let names = myName.split(' ');
+console.log(names);
+
+let firstName = names[0];
+let lastName = names[1];
+console.log(firstName);
+console.log(lastName);
+
+console.log(`Mitt förnamn är ${firstName} och efternamnet är ${lastName} `);  */
+
+
+/*  
+
+  NUMBERS
+
+  ------------------------------------------------------------
+
+  **  Lägg till 
+  -   ta bort
+  *   gångra
+  **  upphöjd i
+  / dela
+  %  division reminder
+  ** öka med 1
+  -- minska med 1
+
+  = lika med
+  += lägga till och ändrar på det ursprungliga värdet
+  -= ta bort från
+  *= gångra och lägga till
+  /* dela och lägg till
+
+
+*/
+
+
+let nr1 = 10;
+let nr2 = 2.5;
+
+console.log(nr1 + nr2);
+
+nr1 = nr1 + 1;
+nr1 += 1;
+nr1 ++;
+
+
+nr1 = nr1 - 1;
+nr1 -= 1;
+nr1 --;
+console.log(nr1);
+console.log(nr1 ** nr2);
+console.log(nr1 % 3);
+
