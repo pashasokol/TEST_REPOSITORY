4737e8313f45949e9ccf1e88da3f476938ca0693
Pavel <sokoliklive@gmail.com>
2021-09-23 08:46:06 +0200

    javascript

--- Lektion 4/3 JS console/script.js
+++ Lektion 4/3 JS console/script.js
@@ -1,4 +1,11 @@
 console.log("Hello")
-// En kommentar såhär
+// En kommentar skrivs såhär (ctrl *)
 
 
+
+
+document.body.innerText = "Detta är en text";
+
+console.log('Här skriver vi ut någonting i konsolen');
+console.warn('Detta är en verning');
+console.error('Detta är ett felmeddelande');
