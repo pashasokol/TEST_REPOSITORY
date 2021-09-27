cfbf80b1061642174fc0ff34305905b12ba67569
Pavel <sokoliklive@gmail.com>
2021-09-27 22:15:33 +0200

    functions

--- Lektion 5/3-functions/script.js
+++ Lektion 5/3-functions/script.js
@@ -1,62 +1,90 @@
 // Scope
 
 
-let age = 34; // globalt scope
+// let age = 34; // globalt scope
 
 
-if(true) {
-    let age = 50;  
-    let name = 'Pavel'; // local scope
-    console.log('inne i 1a kodblocket', age);
-    console.log('inne i 1a kodblocket', name);
+// if(true) {
+//     let age = 50;  
+//     let name = 'Pavel'; // local scope
+//     console.log('inne i 1a kodblocket', age);
+//     console.log('inne i 1a kodblocket', name);
 
 
-    if(true){
+//     if(true){
 
-        let age = 100;
-        let name = 'Jeanette';
-        console.log('inne i kodblock 1.2', age, name);
-        var globalt = 'var';
-    }
-};
+//         let age = 100;
+//         let name = 'Jeanette';
+//         console.log('inne i kodblock 1.2', age, name);
+//         var globalt = 'var';
+//     }
+// };
 
-console.log('utanför något block', age);
-console.log('utanför något block', name);
-
-
+// console.log('utanför något block', age);
+// console.log('utanför något block', name);
 
 
-if(true) {
 
-    let age = 66;
-    let name = 'Hans';
-    console.log('inne i 1a kodblocket', age);
-    console.log('inne i 1a kodblocket', name);
 
+// if(true) {
+
+//     let age = 66;
+//     let name = 'Hans';
+//     console.log('inne i 1a kodblocket', age);
+//     console.log('inne i 1a kodblocket', name);
+
+// }
+
+// console.log('inne i 1a kodblocket', age);
+// console.log('inne i 1a kodblocket', name);
+// console.log('inne i 1a kodblocket', globalt);
+
+
+// // Funktioner
+
+
+// // Deklarera en funktion
+
+// function hej() {
+//     console.log('Hej på dig');
+// }
+
+// const greet = function() {
+//     console.log('hur är läget?')
+// }
+
+// // Kalla på en funktion
+
+// hej();
+// greet();
+
+
+// funktion med parametrar
+
+let firstName = 'Pavel';
+
+
+const greet = function(name) {
+
+    console.log(`Hej ${name} hur är läget?`)
 }
 
-console.log('inne i 1a kodblocket', age);
-console.log('inne i 1a kodblocket', name);
-console.log('inne i 1a kodblocket', globalt);
+// greet();
+
+// firstName = 'Hans';
+
+// greet();
+
+greet('Pavel');
+greet(firstName);
+greet('Hans');
 
 
-// Funktioner
 
-
-// Deklarera en funktion
-
-function hej() {
-    console.log('Hej på dig');
+const func1 = function(firstName, lastName) {
+    let _fullName = `${firstName} ${lastName}`
+    console.log(_fullName.trim());
 }
 
-const greet = function() {
-    console.log('hur är läget?')
-}
 
-hej();
-greet();
-
-
-
-// Kalla på en funktion
-
+func1('Pavel','Sakalouski');
