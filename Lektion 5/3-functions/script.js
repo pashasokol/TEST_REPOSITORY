e266a67f661f0277d708d6358813876bc5e7310e
Pavel <sokoliklive@gmail.com>
2021-09-28 17:34:46 +0200

    function

--- Lektion 5/3-functions/script.js
+++ Lektion 5/3-functions/script.js
@@ -64,20 +64,20 @@
 let firstName = 'Pavel';
 
 
-const greet = function(name) {
+// const greet = function(name) {
 
-    console.log(`Hej ${name} hur är läget?`)
-}
+//     console.log(`Hej ${name} hur är läget?`)
+// }
 
-// greet();
+// // greet();
 
-// firstName = 'Hans';
+// // firstName = 'Hans';
 
-// greet();
+// // greet();
 
-greet('Pavel');
-greet(firstName);
-greet('Hans');
+// greet('Pavel');
+// greet(firstName);
+// greet('Hans');
 
 
 
@@ -88,3 +88,128 @@ const func1 = function(firstName, lastName) {
 
 
 func1('Pavel','Sakalouski');
+
+
+// const calc = function(num1, num2) {
+//     const sum = Number(num1) + Number(num2)
+
+//     if(sum == 'NaN'){
+//         console.log(sum);
+
+
+//     }
+//     else {
+//         console.log('you need to add a valid number')
+//     }
+
+
+// }
+
+// const calc = function(num1, num2) {
+//     const sum = num1 + num2;
+//     console.log(sum);
+// }
+
+// calc(5,10);
+// calc(15,20);
+// calc(15,'hej');
+
+
+
+// const calc = function(num1, num2) {
+//     const sum = num1 + num2;
+//     return sum;
+    
+// }
+
+
+// calc(3,5);
+// console.log(calc(3,5));
+
+
+// // Arrow functions
+
+
+// console.log(this);
+
+
+
+// const greet = (name) => {
+
+//     console.log(`Hej ${name} hur är läget?`)
+// }
+
+// greet('Pavel');
+
+
+// const calc = (num1 = 0, num2 = 0) => {
+
+//     const sum = num1 + num2;
+//     return sum;
+
+// }
+
+const calc = (num1 = 0, num2 = 0) => num1 + num2;
+
+console.log(calc(5,6));
+
+
+
+const greet = name => `Hej ${name} hur är läget?`;
+
+
+console.log(greet('Pavel'));
+
+
+
+// Callback functions
+
+const minFunction = (val, cb) => {
+    let sum = val * 2;
+    // setTimeout(() =>  {
+    //     callbackFunction(sum)
+    // }, 3000)
+    cb(sum);
+}
+
+minFunction(5, summa => {
+    console.log(summa);
+    // let nySumma = summa * 2;
+    // console.log(nySumma);
+} )
+
+
+let names = ['Pavel', 'Sanna', 'Stepa'];
+
+// names.forEach( name => {
+//     console.log(name);
+// })
+
+
+// Metoder
+
+let myName = names[0];
+console.log(myName);
+
+
+let upper = myName.toUpperCase();
+console.log(upper)
+
+
+let user = {
+
+    firstName: 'Pavel',
+    lastName: 'Sakalouski',
+    fullname: function() {
+        console.log(this);
+        return this.firstName + '  '  + this.lastName;        
+    }
+}
+
+
+
+let userName = user.fullname();
+
+console.log(userName);
+
+
